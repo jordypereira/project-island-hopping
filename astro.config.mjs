760bdd65d57bbe644cbe0022b148ea94bf5ea007
Jordy <jordy.pereira@hotmail.com>
2022-07-19 22:32:20 +0200

    Add profiles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), image()]
 });
---
+++ src/data/profiles.ts
@@ -0,0 +1,22 @@
+export const profiles = [
+  {
+    id: 4,
+    alt: "Fiolinist Elia Kaden",
+    text: `Elia Kaden studerte ved Hochschule für Musik „Hanns Eisler“ i Berlin under Kolja Blacher, etterfulgt av en mastergrad med Geir Inge Lotsberg på Barratt Due Musikkinstitutt.  Han har jobbet som vikar og stipendiat med mange forskjellige orkestre, blant annet Oslo-Filharmonien, Den Norske Opera og Ballett, Kringkastingsorkesteret og Deutsches Symphonie-Orchester Berlin. Han jobbet som Kvartettmusiker i mange forskjellige kvartetter, blant annet «Kratos Kvartett», med mentorer som Eberhard Feltz og Christoph Poppen. Han spiller på en Christoph Götting fra 2006.`,
+  },
+  {
+    id: 3,
+    alt: "Fiolinist Emilie Haagenrud",
+    text: `Fiolinist Emilie Haagenrud er en ettertraktet kammer- og orkestermusiker. Hun vikarierer jevnlig i Oslo-Filharmonien, Bergen Filharmoniske Orkester, og Ensemble Allegria. Hun har også vikariert i Operaorkestret i Oslo, Stavanger Symfoniorkester og Danmarks Radios Symfoniorkester. Fra 2020 har hun vært fast medlem av Risør Festival Strings. Emilie Haagenrud har en master i utøvende fiolin fra Norges Musikkhøgskole med Peter Herresthal som lærer, og en Bachelor i utøvende fiolin fra Barratt Due Musikkinstitutt med Geir Inge Lotsberg og Alf Richard Kraggerud som lærere.`,
+  },
+  {
+    id: 2,
+    alt: "Bratsjist Heidi Johnstad Arnesen",
+    text: `Heidi Johnstad Arnesen har sin bachelor- og mastergrad i utøvende musikk fra Barratt Due Musikkinstitutt og Norges Musikkhøgskole, hvor hun har studert bratsj med Soon-Mi Chung, Lars Anders Tomter og Ida Bryhn. Hun jobber som frilans-bratsjist og vikarierer i flere av de norske orkestrene, spiller i mange ulike prosjektbaserte kammerorkester og ensembler, og gjør flere oppdrag innenfor sjangerene jazz, pop og rock.`,
+  },
+  {
+    id: 1,
+    alt: "Cellist Malin Mertens",
+    text: `Malin Mertens spiller cello og studerte utøvende musikk ved Barratt Due musikkinstitutt i Oslo under Kari Ravnan og Ole Eirik Ree, etterfulgt av en mastergrad med Louisa Tuck med avsluttende eksamen i 2019. Under studietiden var hun aktiv som musiker og har opptrådt med flere profesjonelle ensembler og orkestre, inkludert Oslo Filharmonien, Kungliga Filharmonikerna (SE), Kringkastingsorkesteret, Arktisk Filharmoni og Trondheim Symfoniorkester. Hun var en av grunnleggerne av Virentia Strykekvartett, og i løpet av mastergraden spilte hun på en cello utlånt av Dextra Musica. Etter mastergraden jobbet hun ett år med Kungliga Filharmonikerna i Stockholm som orkesterakademist. Siden høsten 2020 jobber hun som frilanser på heltid med base i Oslo, Norge.`,
+  },
+]
