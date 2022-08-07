7059562bb1d0d5db638d89b0d0bb9dfd3e3b95a3
Jordy <dev@jordypereira.be>
2022-08-07 21:58:24 +0200

    Bug with astro images

---
+++ src/components/Profiles.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Image } from "@astrojs/image"
+  import { profiles } from "../data/profiles"
+</script>
+
+<section class="my-8 flex space-y-8 flex-col">
+  {#each profiles as { id, alt, text }, index}
+    <article
+      class="flex justify-center w-full flex-wrap lg:flex-nowrap"
+      class:lg:flex-row-reverse={index % 2 !== 0}
+    >
+      <div>
+        <Image
+          width={192}
+          height={288}
+          class="lg:max-w-48 h-auto"
+          src={import(`../assets/images/profiles/${id}-192w.jpg`)}
+          {alt}
+        />
+      </div>
+      <p
+        class="flex items-center p-4 lg:p-0"
+        class:lg:ml-6={index % 2 === 0}
+        class:lg:mr-6={index % 2 !== 0}
+      >
+        {text}
+      </p>
+    </article>
+  {/each}
+  )) }
+</section>
